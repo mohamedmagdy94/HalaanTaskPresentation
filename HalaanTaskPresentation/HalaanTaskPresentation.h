dc1f19c8cf33a5903794c362827a96a2fc7fc540
Mohamed El-Taweel <mohamedmagdy.9080@gmail.com>
2022-10-20 13:42:35 +0200

    Initial Commit

---
+++ HalaanTaskPresentation/HalaanTaskPresentation.h
@@ -0,0 +1,18 @@
+//
+//  HalaanTaskPresentation.h
+//  HalaanTaskPresentation
+//
+//  Created by Mohamed on 20/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HalaanTaskPresentation.
+FOUNDATION_EXPORT double HalaanTaskPresentationVersionNumber;
+
+//! Project version string for HalaanTaskPresentation.
+FOUNDATION_EXPORT const unsigned char HalaanTaskPresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HalaanTaskPresentation/PublicHeader.h>
+
+
